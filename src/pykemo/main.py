2bfc65ca9f82c80d91b309ab62c45c8d31acd076
The Silent One <nlsgtres@gmail.com>
2024-08-21 23:38:58 -0300

    docs: Creating doc files

--- src/pykemo/main.py
+++ src/pykemo/main.py
@@ -5,7 +5,5 @@ Main module. If pykemo is executed in console, it will lead to this module.
 
 def main(*args) -> int:
     "Main function"
-    
-    print("DOC generation functionality to be added at a later date...")
 
     return 0
